053db51a2103c3267310e19ec69635a902122414
Vijayalakshmi Velur <velur.viju@gmail.com>
2019-08-30 14:34:45 +0530

    [Vij,Chitra]Modified. Equal functionality

--- src/com.tw.vapasi/main/DistanceMeasurement.java
+++ src/com.tw.vapasi/main/DistanceMeasurement.java
@@ -1,15 +1,35 @@
 //Understand dimensions
-public class DistanceMeasurement {
+    class DistanceMeasurement {
 
     private double value;
     private String units;
-    public DistanceMeasurement(double value, String units) {
+    DistanceMeasurement(double value, String units) {
         this.value = value;
         this.units = units;
     }
 
-    public boolean equals(DistanceMeasurement measurementToCompare) {
-        return true;
+    boolean equals(DistanceMeasurement measurementToCompare) {
+        if( this == measurementToCompare )
+            return true;
+        if( measurementToCompare == null || this.getClass() != measurementToCompare.getClass() )
+            return false;
+        if( this.units.equals( measurementToCompare.units ))
+        {
+            return this.value == measurementToCompare.value;
+        }
+        return convertToCM( this ).value == ( convertToCM( measurementToCompare ).value);
+
+
+    }
+    private DistanceMeasurement convertToCM( DistanceMeasurement measurement)
+    {
+        if( measurement.units.equals("m")) {
+            return new DistanceMeasurement( measurement.value * 100,"cm");
+        }
+        if( measurement.units.equals("km")) {
+            return new DistanceMeasurement(measurement.value * 100000,"cm");
+        }
+        return measurement;
 
     }
 }
--- src/test/com.tw.vapasi/test/DistanceMeasurementTest.java
+++ src/test/com.tw.vapasi/test/DistanceMeasurementTest.java
@@ -3,20 +3,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DistanceMeasurementTest {
+    class DistanceMeasurementTest {
     @Test
-
-    public void expectTrueFor100CMand1M() {
+    void expectTrueFor100CMand1M() {
         DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
         DistanceMeasurement meter1 = new DistanceMeasurement(1,"m");
 
         assertTrue(centimeter100.equals(meter1));
     }
     @Test
-    public void expectFalseFor100CMand1M() {
+    void expectTrueFor100CMand100CM() {
+        DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
+        DistanceMeasurement centimeter100Other = new DistanceMeasurement(100,"cm");
+
+        assertTrue(centimeter100.equals(centimeter100Other));
+    }
+    @Test
+    void expectFalseFor100CMand1KM() {
         DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
         DistanceMeasurement kilometer1 = new DistanceMeasurement(1,"km");
 
         assertFalse(centimeter100.equals(kilometer1));
+    }
+
+    @Test
+    void expectTrueFor1000Mand1KM() {
+        DistanceMeasurement meter1000 = new DistanceMeasurement(1000,"m");
+        DistanceMeasurement kilometer1 = new DistanceMeasurement(1,"km");
+
+        assertTrue(meter1000.equals(kilometer1));
     }
 }
