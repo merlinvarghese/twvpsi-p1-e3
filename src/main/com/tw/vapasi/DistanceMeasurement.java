ba93487800f5a5961174a3b819c2aac31df6a0b4
Vijayalakshmi Velur <velur.viju@gmail.com>
2019-08-30 20:58:39 +0530

    [Vij,Chitra]Modified. Incorporated Enum for Units

--- src/main/com/tw/vapasi/DistanceMeasurement.java
+++ src/main/com/tw/vapasi/DistanceMeasurement.java
@@ -1,52 +1,44 @@
 package com.tw.vapasi;
 
 //Understand dimensions
-    class DistanceMeasurement {
+class DistanceMeasurement {
 
     private double value;
-    private String units;
-    //private final Unit units;
-
-    private final String UNIT_CENTIMETER = "cm";
-    private final String UNIT_METER = "m";
-    private final String UNIT_KILOMETER = "km";
+    private final Unit units;
     private final int VALUE_ONELAKH = 100000;
     private final int VALUE_HUNDRED = 100;
 
-    enum Unit{
+    enum Unit {
         KM,
         CM,
         M
     }
 
-    DistanceMeasurement(double value, String units) {
+    DistanceMeasurement(double value, Unit units) {
         this.value = value;
         this.units = units;
     }
 
-    private double convertToCentimeter()
-    {
-        if( this.units.equals(UNIT_METER)) {
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null || this.getClass() != object.getClass())
+            return false;
+        DistanceMeasurement measurementToCompare = (DistanceMeasurement) object;
+        if (this.units.equals(measurementToCompare.units)) {
+            return this.value == measurementToCompare.value;
+        }
+        return this.convertToCentimeter() == measurementToCompare.convertToCentimeter();
+    }
+
+    private double convertToCentimeter() {
+        if (this.units.equals(Unit.M)) {
             return this.value * VALUE_HUNDRED;
         }
-        if( this.units.equals(UNIT_KILOMETER)) {
+        if (this.units.equals(Unit.KM)) {
             return this.value * VALUE_ONELAKH;
         }
         return this.value;
-
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if( this == object )
-            return true;
-        if( object == null || this.getClass() != object.getClass() )
-            return false;
-        DistanceMeasurement measurementToCompare = (DistanceMeasurement)object;
-        if( this.units.equals( measurementToCompare.units ))
-        {
-            return this.value == measurementToCompare.value;
-        }
-        return this.convertToCentimeter() == measurementToCompare.convertToCentimeter() ;
     }
 }
--- src/test/com/tw/vapasi/DistanceMeasurementTest.java
+++ src/test/com/tw/vapasi/DistanceMeasurementTest.java
@@ -1,63 +1,64 @@
 package com.tw.vapasi;
 
-import com.tw.vapasi.DistanceMeasurement;
+import com.tw.vapasi.DistanceMeasurement.Unit;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-    class DistanceMeasurementTest {
+class DistanceMeasurementTest {
 
     @Test
     void expectFalseFor100CMandNull() {
-        DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
+        DistanceMeasurement centimeter100 = new DistanceMeasurement(100, Unit.CM);
         assertFalse(centimeter100.equals(null));
     }
 
     @Test
     void expectTrueFor100CMandSameReference() {
-        DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
+        DistanceMeasurement centimeter100 = new DistanceMeasurement(100, Unit.CM);
 
         assertTrue(centimeter100.equals(centimeter100));
     }
 
     @Test
     void expectTrueFor100CMand100CM() {
-        DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
-        DistanceMeasurement centimeter100Other = new DistanceMeasurement(100,"cm");
+
+        DistanceMeasurement centimeter100 = new DistanceMeasurement(100, Unit.CM);
+        DistanceMeasurement centimeter100Other = new DistanceMeasurement(100, Unit.CM);
 
         assertTrue(centimeter100.equals(centimeter100Other));
     }
 
     @Test
     void expectTrueFor100CMand200CM() {
-        DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
-        DistanceMeasurement centimeter100Other = new DistanceMeasurement(200,"cm");
+        DistanceMeasurement centimeter100 = new DistanceMeasurement(100, Unit.CM);
+        DistanceMeasurement centimeter100Other = new DistanceMeasurement(200, Unit.CM);
 
         assertFalse(centimeter100.equals(centimeter100Other));
     }
 
     @Test
     void expectTrueFor100CMand1M() {
-        DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
-        DistanceMeasurement meter1 = new DistanceMeasurement(1,"m");
+        DistanceMeasurement centimeter100 = new DistanceMeasurement(100, Unit.CM);
+        DistanceMeasurement meter1 = new DistanceMeasurement(1, Unit.M);
 
         assertTrue(centimeter100.equals(meter1));
     }
 
     @Test
     void expectFalseFor100CMand1KM() {
-        DistanceMeasurement centimeter100 = new DistanceMeasurement(100,"cm");
-        DistanceMeasurement kilometer1 = new DistanceMeasurement(1,"km");
+        DistanceMeasurement centimeter100 = new DistanceMeasurement(100, Unit.CM);
+        DistanceMeasurement kilometer1 = new DistanceMeasurement(1, Unit.KM);
 
         assertFalse(centimeter100.equals(kilometer1));
     }
 
     @Test
     void expectTrueFor1000Mand1KM() {
-        DistanceMeasurement meter1000 = new DistanceMeasurement(1000,"m");
-        DistanceMeasurement kilometer1 = new DistanceMeasurement(1,"km");
+        DistanceMeasurement meter1000 = new DistanceMeasurement(1000, Unit.M);
+        DistanceMeasurement kilometer1 = new DistanceMeasurement(1, Unit.KM);
 
         assertTrue(meter1000.equals(kilometer1));
     }
